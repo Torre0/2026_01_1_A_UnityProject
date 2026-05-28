2018160b5afac6574afb4120ecd876b9166a71d7
Torre0 <obo0512obo@gmail.com>
2026-05-28 11:50:47 +0900

    [Feat] 다이얼로그 제작

---
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,162 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class DialogueManager : MonoBehaviour
+{
+    [Header("UI 요소 - 인스펙터 창에서 연결")]
+    public GameObject DialoguePanel;
+    public Image characterImage;
+    public TextMeshProUGUI characterNaneText;
+    public TextMeshProUGUI dialogueText;
+    public Button nextButton;
+
+    [Header("기본 설정")]
+    public Sprite defaultCharacterImage;
+
+    [Header("타이핑 효과 설정")]
+    public float typingSpeed = 0.05f;
+    public bool skipTypingOnClick = true;
+
+    private DialogueDataSO currentDialogue;
+    private int currenLineIndex = 0;
+    private bool isDialogueActive = false;
+    private bool isTyping = false;
+    private Coroutine typingCoroutine;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DialoguePanel.SetActive(false);
+        nextButton.onClick.AddListener(HandleNextInput);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDialogueActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            HandleNextInput();
+        }
+    }
+
+    IEnumerator TypeText(string textToType)
+    {
+        isTyping = true;
+        dialogueText.text = "";
+
+        for (int i = 0; i < textToType.Length; i++)
+        {
+            dialogueText.text += textToType[i];
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        isTyping = false;
+    }
+
+    private void CompleteTyping()
+    {
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+
+        isTyping = false;
+
+        if (currentDialogue != null && currenLineIndex < currentDialogue.dialogueLines.Count)
+        {
+            dialogueText.text = currentDialogue.dialogueLines[currenLineIndex];
+        }
+    }
+
+    void ShowCurrentLine()
+    {
+        if (currentDialogue != null && currenLineIndex < currentDialogue.dialogueLines.Count)
+        {
+            if(typingCoroutine != null)
+            {
+                StopCoroutine(typingCoroutine);
+            }
+        }
+
+        string currentText = currentDialogue.dialogueLines[currenLineIndex];
+        typingCoroutine = StartCoroutine(TypeText(currentText));
+    }
+
+    public void ShowNextLine()
+    {
+        currenLineIndex++;
+
+        if (currenLineIndex >= currentDialogue.dialogueLines.Count)
+        {
+            EndDialogue();
+        }
+        else
+        {
+            ShowCurrentLine();
+        }
+    }
+
+    void EndDialogue()
+    {
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+            typingCoroutine = null;
+        }
+
+        isDialogueActive = false;
+        isTyping = false;
+        DialoguePanel.SetActive(false);
+        currenLineIndex = 0;
+    }
+
+    public void HandleNextInput()
+    {
+        if (isTyping && skipTypingOnClick)
+        {
+            CompleteTyping();
+        }
+        else if (!isTyping)
+        {
+            ShowNextLine();
+        }
+    }
+
+    public void SkipDialogue()
+    {
+        EndDialogue();
+    }
+
+    public bool IsDialgueActive()
+    {
+        return isDialogueActive;
+    }
+
+    public void StartDialogue(DialogueDataSO dialogue)
+    {
+        if (dialogue == null || dialogue.dialogueLines.Count == 0) return;
+
+        currentDialogue = dialogue;
+        currenLineIndex = 0;
+        isDialogueActive = true;
+
+        DialoguePanel.SetActive(true);
+        characterNaneText.text = dialogue.characterName;
+
+        if (characterImage != null)
+        {
+            if(dialogue.characterImage != null)
+            {
+                characterImage.sprite = dialogue.characterImage;
+            }
+            else
+            {
+                characterImage.sprite = defaultCharacterImage;
+            }
+        }
+
+        ShowCurrentLine();
+    }
+}
